1ec26d28139b77bec1bebc520a4d6013ad0d9cdd
LACRIM94 <42764406+LACRIM94@users.noreply.github.com>
2019-11-13 12:25:17 +0100

    Add files via upload
    Kontroll och funktionalitet klasser tillagda

---
+++ Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void processNum(ActionEvent event) {
+
+    }
+
+    public void processOperator(ActionEvent event) {
+
+    }
+}
---
+++ Model.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class Model {
+    public float count (long num1 ,long num2 , String operator) {
+        return 0;
+    }
+}
