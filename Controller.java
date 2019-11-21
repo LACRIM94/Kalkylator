1b55936e1c3998309c0c84f585a479686c8594ba
LACRIM94 <42764406+LACRIM94@users.noreply.github.com>
2019-11-21 13:47:26 +0100

    Add files via upload
    kopplade fxml med controll

--- Controller.java
+++ Controller.java
@@ -1,14 +1,29 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class Controller {
 
+    @FXML
+    private Label result;
+    private long num1 = 0;
+    private String operator = "";
+    private boolean start = true;
+
+
+    @FXML
     public void processNum(ActionEvent event) {
 
     }
 
+    @FXML
     public void processOperator(ActionEvent event) {
+
+
+        }
+
 
     }
 }
