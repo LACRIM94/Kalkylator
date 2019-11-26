38537616fcbaa331595adc793a9e3dbad698812e
LACRIM94 <42764406+LACRIM94@users.noreply.github.com>
2019-11-26 21:34:15 +0100

    Add files via upload
    Har fixat input av siffror på miniräknaren

--- Controller.java
+++ Controller.java
@@ -12,6 +12,7 @@ public class Controller {
     private long num1 = 0;
     private String operator = "";
     private boolean start = true;
+    private Model model = new Model();
 
 
     @FXML
@@ -29,10 +30,30 @@ public class Controller {
 
     @FXML
     public void processOperator(ActionEvent event) {
+        String value = ((Button)event.getSource()).getText();
 
+        if (!value.equals("=")) {
+            if (operator.isEmpty())
+                return;
+
+            operator = value;
+            num1 = Long.parseLong(result.getText());
+            result.setText("");
+        } else {
+            if (operator.isEmpty())
+                return;
+            long num2 = Long.parseLong(result.getText());
+            float output = model.count(num1, num2, operator);
+            result.setText(String.valueOf(output));
+            operator = "";
+            start = true;
 
         }
 
 
+
     }
-}
+
+
+    }
+
